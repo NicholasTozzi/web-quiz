c55aca43399a777c7ca7f884faccdea0f4472c4d
Tozzi <nicholastozzi@gmail.com>
2023-08-09 20:19:30 -0400

    added foundation for quiz js file

---
+++ quiz.js
@@ -0,0 +1,52 @@
+//?es6 function    const x = (x, y) => x * y; or => {}
+
+//query selectors
+
+//container selectors
+
+//Questions
+//! Multiple Choice
+
+//! Bubble Choice
+
+//* Functions
+
+function Init() {
+return;
+    }  Init();
+
+function startQuiz() {
+    return;  
+}
+
+function displayQuestion() {
+    return;
+}
+
+function displayProgress(){
+ return;
+}
+
+function verifyAnswer(){
+    return;
+   }
+
+function logIncorrect(){
+    return
+}
+
+function retry(){
+    return;
+   }
+
+function submitQuiz(){
+//when portal is created this function will change slightly    
+}
+
+
+// event listeners
+
+//? es6 event listeners
+
+
+
